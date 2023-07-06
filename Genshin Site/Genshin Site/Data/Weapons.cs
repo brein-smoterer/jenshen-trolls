b978e2b75476da54dd5addc91f69e66f273682e5
Aleks <majakolevatzvetkova@gmail.com>
2023-07-06 15:01:35 +0300

    werpon class

--- Genshin Site/Genshin Site/Data/Weapons.cs
+++ Genshin Site/Genshin Site/Data/Weapons.cs
@@ -1,6 +1,14 @@
 ﻿namespace Genshin_Site.Data
 {
-    public class Weapons
+    public class Weapon
     {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Substat { get; set; }
+        public string Passive { get; set; }
+        public Weapon(string name, string type, string substat, string passive)
+        {
+           
+        }
     }
 }
