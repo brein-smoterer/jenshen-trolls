abf3103499536338b0bd2b7819fe5ef976ae9f27
Християн Лазаров <hl05089847@edu.mon.bg>
2023-07-07 11:34:21 +0300

    Made the character page display only character names and reveal extra infro on click

--- Genshin Site/Genshin Site/Data/Characters.cs
+++ Genshin Site/Genshin Site/Data/Characters.cs
@@ -5,12 +5,14 @@
         public string CharacterName { get; }
         public string Element { get; }
         public string Weapon { get; }
+        public bool Show { get; set; }
 
-        public Character(string CharacterName1, string Element1, string Weapon1)
+        public Character(string CharacterName1, string Element1, string Weapon1, bool Show1)
         {
             CharacterName = CharacterName1;
             Element = Element1;
             Weapon = Weapon1;
+            Show = false;
         }
     }
 }
