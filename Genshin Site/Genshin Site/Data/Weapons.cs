3c09dad7bbccf8f85092d51bbc5c93bf5892371b
Aleks <majakolevatzvetkova@gmail.com>
2023-07-07 17:30:00 +0300

    images weapons add 3

--- Genshin Site/Genshin Site/Data/Weapons.cs
+++ Genshin Site/Genshin Site/Data/Weapons.cs
@@ -1,18 +1,20 @@
 ﻿namespace Genshin_Site.Data
 {
     public class Weapon
-    {
+    { 
+        public string Image { get; }
         public bool Show = false;
         public string Name { get; }
         public string Type { get; }
         public string Substat { get; }
         public string Passive { get; }
-        public Weapon(string name, string type, string substat, string passive)
+        public Weapon(string name, string type, string substat, string passive, string image)
         {
             Name = name;
             Type = type;
             Substat = substat;
             Passive = passive;
+            Image = image;
         }
     }
 }
