b9c09dfdf6ee759eb694f8a01223433852dface4
Боил Василев <boil4o@abv.bg>
2023-07-10 10:15:43 +0300

    Added artifact set images Made Show function for artifact sets work on image and text

--- Genshin Site/Genshin Site/Data/ArtifactSet.cs
+++ Genshin Site/Genshin Site/Data/ArtifactSet.cs
@@ -2,13 +2,16 @@
 {
     public class ArtifactSet
     {
+        public string Img;
+        //public FileInfo[] files = d.GetFiles();
         public string ArtifactSetName { get; }
         public string ArtifactSetPassive { get; }
         public bool Show = false;
-        public ArtifactSet(string artifactSetName, string artifactSetPassive)
+        public ArtifactSet(string artifactSetName, string artifactSetPassive, string img)
         {
             ArtifactSetName = artifactSetName;
             ArtifactSetPassive = artifactSetPassive;
+            Img = img;
         }
     }
 }
